a40f82583bdb951f9b120c17bdf802189881ac0a
Louis MARTIN <louisrtm@gmail.com>
2017-02-08 20:06:31 +0100

    Improve run.sh, to merge all output files

--- a1/code/run.sh
+++ a1/code/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-rm -rf out *.class *.jar;
+rm -rf .out* out* *.class *.jar;
 hadoop com.sun.tools.javac.Main $1.java;
 jar cf run.jar $1*.class;
 hadoop jar run.jar $1 ../data/ out;
-cat out/part-r-00000
+hadoop fs -getmerge out out.csv;
+cat out.csv;
