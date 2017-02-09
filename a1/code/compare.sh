b4bd2db805094b3efcadcb3388288a12b7ff1e74
Louis MARTIN <louisrtm@gmail.com>
2017-02-09 11:48:54 +0100

    Split run.sh into compile.sh and compare.sh

---
+++ a1/code/compare.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+./compile.sh StopWords
+
+# Usage:
+# hadoop jar myfile.jar Class input_dir output_dir n_reducers combiner compression
+
+# 10 reducers no combiner
+rm -rf .out* out*
+hadoop jar StopWords.jar StopWords ../data/ out 10 0 0;
+hadoop fs -getmerge out stopwords.csv;
+
+# 10 reducers , 1 combiner
+rm -rf .out* out*
+hadoop jar run.jar StopWords.jar ../data/ out 10 1 0;
+hadoop fs -getmerge out stopwords.csv;
+
+# 10 reducers , 1 combiner, compression
+rm -rf .out* out*
+hadoop jar run.jar StopWords.jar ../data/ out 10 1 1;
+hadoop fs -getmerge out stopwords.csv;
+
+# 50 reducers , 1 combiner, compression
+rm -rf .out* out*
+hadoop jar run.jar StopWords.jar ../data/ out 10 1 1;
+hadoop fs -getmerge out stopwords.csv;
