c30427bbe6bccb4e96bed1c6007f5604c7e0ae43
Louis MARTIN <louisrtm@gmail.com>
2017-02-09 20:25:43 +0100

    Add InvertedIndex compiler script

---
+++ a1/code/run_inverted.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./compile.sh InvertedIndex
+
+# Usage:
+# hadoop jar myfile.jar Class input_dir output_dir n_reducers combiner compression
+
+rm -rf .out* out*
+hadoop jar InvertedIndex.jar InvertedIndex ../example/ out 10 1 0;
+hadoop fs -getmerge out invertedindex.csv;
