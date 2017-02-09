10db18609f95aeabafbd9e252423ce61e2ecbbb6
Louis MARTIN <louisrtm@gmail.com>
2017-02-09 20:23:49 +0100

    First working version of inverted index

--- a1/code/InvertedIndex.java
+++ a1/code/InvertedIndex.java
@@ -9,77 +9,78 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class StopWords {
+
+public class InvertedIndex {
 
   public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, IntWritable>{
-
-    private final static IntWritable one = new IntWritable(1);
+       extends Mapper<Object, Text, Text, Text>{
     private Text word = new Text();
+    private Text file = new Text();
 
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
+      // Get the filename (taken from stackoverflow)
+      FileSplit fileSplit = (FileSplit) context.getInputSplit();
+      String filename = fileSplit.getPath().getName();
+      file.set(filename);
+
       // Splits a string to tokens (here words)
-      StringTokenizer itr = new StringTokenizer(value.toString());
+      StringTokenizer itr = new StringTokenizer(value.toString(), " .,?!");
       while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
+        word.set(itr.nextToken().toLowerCase().trim());
         // Write one (key, value) pair to context
-        context.write(word, one);
+        context.write(word, file);
       }
     }
   }
 
-  public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable(10);
+  public static class PostingListReducer
+       extends Reducer<Text, Text, Text, Text> {
+    private Text result = new Text();
 
-    public void reduce(Text key, Iterable<IntWritable> values,
+    public void reduce(Text key, Iterable<Text> values,
                        Context context
                        ) throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
+
+      String postingList = new String();
+      for (Text val : values) {
+        postingList += val + ", ";
       }
-      // Only write the key value pair if its frequency is high enough
-      if (sum > 4000){
-        result.set(sum);
-        context.write(key, result);
-      }
+      // Remove last two characters of string
+      postingList = postingList.substring(0, postingList.length()-2);
+      result.set(postingList);
+      context.write(key, result);
     }
   }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    // Ressource manager configurations
-    //conf.set("fs.defaultFS", "hdfs://127.0.0.1:9000");
-    //conf.set("mapreduce.jobtracker.address", "localhost:54311");
-    //conf.set("mapreduce.framework.name", "yarn");
-    //conf.set("yarn.resourcemanager.address", "localhost:8032");
 
     // Set separator to write as a csv file
-    conf.set("mapred.textoutputformat.separator", ", ");
+    conf.set("mapred.textoutputformat.separator", ": ");
     // Set compression
     if ((args.length >= 5) && (Integer.parseInt(args[4]) == 1)) {
         conf.set("mapreduce.map.output.compress", "true");
     }
 
     Job job = Job.getInstance(conf, "stop words");
-    job.setJarByClass(StopWords.class);
+    job.setJarByClass(InvertedIndex.class);
 
     // Set number of reducers and combiner through cli
     if (args.length >= 3) {
         job.setNumReduceTasks(Integer.parseInt(args[2]));
     }
     if ((args.length >= 4) && (Integer.parseInt(args[3]) == 1)) {
-        job.setCombinerClass(IntSumReducer.class);
+        job.setCombinerClass(PostingListReducer.class);
     }
     job.setMapperClass(TokenizerMapper.class);
-    job.setReducerClass(IntSumReducer.class);
+    job.setReducerClass(PostingListReducer.class);
 
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     long startTime = System.nanoTime();
