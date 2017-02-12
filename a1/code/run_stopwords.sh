800fa971b6bf8a0978bd6382e035645dfaad3d55
Louis MARTIN <louisrtm@gmail.com>
2017-02-12 18:23:18 +0100

    Update run_stopwords.sh to run on HDFS

--- a1/code/run_stopwords.sh
+++ a1/code/run_stopwords.sh
@@ -5,21 +5,17 @@
 # hadoop jar myfile.jar Class input_dir output_dir n_reducers combiner compression
 
 # 10 reducers no combiner
-rm -rf .out* out*
 hadoop jar StopWords.jar StopWords data/corpus/ out 10 0 0;
-hadoop fs -getmerge out stopwords.csv;
+hdfs dfs -getmerge out stopwords.csv;
 
-# 10 reducers , 1 combiner
-rm -rf .out* out*
+# 10 reducers, 1 combiner
 hadoop jar StopWords.jar StopWords data/corpus/ out 10 1 0;
-hadoop fs -getmerge out stopwords.csv;
+hdfs dfs -getmerge out stopwords.csv;
 
-# 10 reducers , 1 combiner, compression
-rm -rf .out* out*
+# 10 reducers, 1 combiner, compression
 hadoop jar StopWords.jar StopWords data/corpus/ out 10 1 1;
-hadoop fs -getmerge out stopwords.csv;
+hdfs dfs -getmerge out stopwords.csv;
 
-# 50 reducers , 1 combiner, compression
-rm -rf .out* out*
-hadoop jar StopWords.jar StopWords data/corpus/ out 10 1 1;
-hadoop fs -getmerge out stopwords.csv;
+# 50 reducers, 1 combiner, compression
+hadoop jar StopWords.jar StopWords data/corpus/ out 50 1 1;
+hdfs dfs -getmerge out stopwords.csv;
