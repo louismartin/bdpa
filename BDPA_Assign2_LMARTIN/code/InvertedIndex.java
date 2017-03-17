e65a4b6d3e1efd2fbcc45c1b58045910b1ed0a8e
Louis MARTIN <louisrtm@gmail.com>
2017-03-17 15:23:46 +0100

    Working version of fast similarity with inverted index

--- BDPA_Assign2_LMARTIN/code/InvertedIndex.java
+++ BDPA_Assign2_LMARTIN/code/InvertedIndex.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.HashSet;
-import java.util.HashMap;
+import java.lang.Math;
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -28,103 +27,105 @@ import org.apache.hadoop.fs.FSDataOutputStream;
 
 
 public class InvertedIndex {
-
-  public static enum WordCounter {
-    WORDS_IN_UNIQUE_DOC
-  };
+  public static double threshold = 0.5;
 
 
-  public static class PostingListWritable extends MapWritable {
-    // Creates a custom class that overrides the toString method to pretty
-    // print the posting list
-    @Override
-    public String toString() {
-      String stringRepr = new String();
-      for (PostingListWritable.Entry<Writable, Writable> entry : this.entrySet()) {
-        stringRepr += entry.getKey() + "#" + entry.getValue() + ", ";
+  public static HashMap<Long, String> readAllDocs() {
+      // Read all documents into a HashMap for fast element retrieval.
+      // The underlying hashtable structure provides O(1) element retrival.
+      HashMap<Long, String> docs = new HashMap<Long, String>();
+
+      // Read csv file: inspired from https://www.mkyong.com/java/how-to-read-and-parse-csv-file-in-java/
+      String csvFile = "preprocess.csv";
+      BufferedReader br = null;
+      String line = "";
+
+      try {
+          br = new BufferedReader(new FileReader(csvFile));
+          while ((line = br.readLine()) != null) {
+              // use comma as separator
+              String[] splittedLine = line.split(",");
+              docs.put(Long.parseLong(splittedLine[0]), splittedLine[1].trim());
+          }
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      } catch (IOException e) {
+          e.printStackTrace();
+      } finally {
+          if (br != null) {
+              try {
+                  br.close();
+              } catch (IOException e) {
+                  e.printStackTrace();
+              }
+          }
       }
-      // Remove last comma and space of string
-      stringRepr = stringRepr.substring(0, stringRepr.length()-2);
-      return stringRepr;
-    }
+      return docs;
+  }
+
+
+  public static float jaccard(String doc1, String doc2){
+    Set words1 = new HashSet<String>(Arrays.asList(doc1.split(" ")));
+    Set words2 = new HashSet<String>(Arrays.asList(doc2.split(" ")));
+    Set intersection = new HashSet<String>(words1);
+    intersection.retainAll(words2);
+    Set union = new HashSet<String>(words1);
+    union.addAll(words2);
+    float similarity = (float) intersection.size() / (float) union.size();
+    return similarity;
   }
 
 
   public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, PostingListWritable>{
+       extends Mapper<Object, Text, Text, LongWritable>{
     private Text word = new Text();
-    private LongWritable lineKey = new LongWritable();
-    private IntWritable one = new IntWritable(1);
-    private PostingListWritable postingList = new PostingListWritable();
+    private LongWritable docKey = new LongWritable();
 
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
       String[] split = value.toString().split(",");
-      lineKey.set(Long.parseLong(split[0]));
+      docKey.set(Long.parseLong(split[0]));
       // Splits a string to tokens (here words)
-      StringTokenizer itr = new StringTokenizer(split[1], " ");
-      while (itr.hasMoreTokens()) {
-          word.set(itr.nextToken());
-          // Output is a PostingListWritable containing the filename and the value 1
-          postingList.clear();
-          postingList.put(lineKey, one);
-          // Write one (key, value) pair to context
-          context.write(word, postingList);
+      String[] words = split[1].split(" ");
+      int cutoff = words.length - (int) Math.ceil((double) words.length * threshold) + 1;
+      for (int i=0; i < cutoff; i++) {
+          word.set(words[i]);
+          context.write(word, docKey);
       }
-    }
-  }
-
-
-  public static class PostingListCombiner
-       extends Reducer<Text, PostingListWritable, Text, PostingListWritable> {
-    private PostingListWritable postingList = new PostingListWritable();
-    protected boolean isLastWordInUniqueDoc = false;
-
-    public void reduce(Text word, Iterable<PostingListWritable> postingLists,
-                       Context context
-                       ) throws IOException, InterruptedException {
-      // We are going to aggregate all posting lists together
-      postingList.clear();
-      // Iterate through all posting lists comming from the mappers or the combiners
-      for (PostingListWritable mw : postingLists) {
-        // Iterate through the entries of one given posting list
-        for (PostingListWritable.Entry<Writable, Writable> entry : mw.entrySet()) {
-          LongWritable lineKey = (LongWritable) entry.getKey();
-          IntWritable newValue = (IntWritable) entry.getValue();
-          if (postingList.containsKey(lineKey)) {
-            IntWritable oldValue = (IntWritable) postingList.get(lineKey);
-            postingList.put(lineKey, new IntWritable(oldValue.get() + newValue.get()));
-          } else {
-            postingList.put(lineKey, new IntWritable(newValue.get()));
-          }
-        }
-      }
-
-      isLastWordInUniqueDoc = (postingList.size() == 1);
-      context.write(word, postingList);
     }
   }
 
 
   public static class PostingListReducer
-       extends PostingListCombiner {
-    // The only thing different between the reducer and the combiner is the
-    // counter incrementation. It must be incremented only once per key, hence in
-    // the reducer.
-    @Override
-    public void reduce(Text word, Iterable<PostingListWritable> postingLists,
+       extends Reducer<Text, LongWritable, Text, Text> {
+    private Text outputKey = new Text();
+    private Text outputValue = new Text();
+    private HashMap<Long, String> allDocs = readAllDocs();
+
+    public void reduce(Text word, Iterable<LongWritable> docKeys,
                        Context context
                        ) throws IOException, InterruptedException {
-         super.reduce(word, postingLists, context);
-         if (isLastWordInUniqueDoc) {
-           // Increment counter for word appearing in a single document only
-           // We are guaranteed that this is the only time that the counter
-           // will be incremented for this word because all the values from
-           // a given key all go to the same reduce call.
-           context.getCounter(WordCounter.WORDS_IN_UNIQUE_DOC).increment(1);
-         }
-       }
-     }
+      ArrayList<LongWritable> processedKeys = new ArrayList<LongWritable>();
+      String doc1 = new String();
+      String doc2 = new String();
+
+      for (LongWritable docKey : docKeys) {
+        for (LongWritable processedKey : processedKeys) {
+          doc1 = allDocs.get(processedKey.get());
+          doc2 = allDocs.get(docKey.get());
+          float similarity = InvertedIndex.jaccard(doc1, doc2);
+          if (similarity > threshold){
+            outputKey.set("(" + processedKey.toString() + ", " + docKey.toString() + ")");
+            outputValue.set(similarity + "\t\"" + doc1 + "\" - \"" + doc2 + "\"");
+            context.write(outputKey, outputValue);
+          }
+        }
+
+        LongWritable processedKey = new LongWritable(docKey.get());
+        processedKeys.add(processedKey);
+      }
+    }
+  }
 
 
   public static void main(String[] args) throws Exception {
@@ -148,7 +149,7 @@ public class InvertedIndex {
     job.setReducerClass(PostingListReducer.class);
 
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(PostingListWritable.class);
+    job.setOutputValueClass(LongWritable.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     long startTime = System.nanoTime();
@@ -157,23 +158,6 @@ public class InvertedIndex {
       float duration = (endTime - startTime);
       duration /= 1000000000;
       System.out.println("***** Elapsed: " + duration + "s *****\n");
-
-      // Retrieve unique words from counter
-      Counters counters = job.getCounters();
-      Counter uniqueKeysCounter = counters.findCounter(TaskCounter.REDUCE_INPUT_GROUPS);
-      System.out.println("Unique words: " + uniqueKeysCounter.getValue());
-      Counter wordInUniqueFileCounter = counters.findCounter(WordCounter.WORDS_IN_UNIQUE_DOC);
-      String message = wordInUniqueFileCounter.getDisplayName() + ": " + wordInUniqueFileCounter.getValue();
-      System.out.println(message);
-
-      // Write the count of unique words to a file in HDFS
-      Path filePath = new Path("words_in_unique_file.txt");
-      if (hdfs.exists(filePath)) {
-          hdfs.delete(filePath, true);
-      }
-      FSDataOutputStream fin = hdfs.create(filePath);
-      fin.writeUTF(message);
-      fin.close();
 
       System.exit(0);
     }
